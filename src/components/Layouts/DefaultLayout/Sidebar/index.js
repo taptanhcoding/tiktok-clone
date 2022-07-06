f36dbb867c72f49eec557a044c9b0747ede6ed46
taptanhcoding <16520129minh@gmail.com>
2022-07-06 21:32:57 +0700

    css layout

--- src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,11 +1,14 @@
-import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
 
+import { Link } from 'react-router-dom';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 function Sidebar() {
     return (
-        <>
+        <aside className={cx('wrapper')}>
             <h2>Sidebar</h2>
-            <Link to="/following">to Following</Link>
-        </>
+        </aside>
     );
 }
 
